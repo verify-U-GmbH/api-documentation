13b77c54e5e30d1b818c64c7fc3ed911a3309b28
Norbert Sprang <norbert.sprang@verify-u.com>
2020-04-16 14:55:05 +0200

    Display API reference without layout

--- lib/web/index/index_controller.ex
+++ lib/web/index/index_controller.ex
@@ -1,7 +1,11 @@
 defmodule Web.IndexController do
   use Web, :controller
 
+  def index(conn, %{"name" => "reference"}) do
+    conn = put_layout(conn, false)
+    render(conn, "reference.html")
+  end
+
   def index(conn, %{"name" => name}), do: render(conn, "#{name}.html")
   def index(conn, %{}), do: render(conn, "index.html")
-
 end
