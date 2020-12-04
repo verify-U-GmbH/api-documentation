f760d3c6e1fd9765c5d486458551e95c4105c27b
anatoly <anatolyostrovsky@gmail.com>
2020-12-04 15:41:43 +0200

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Server.MixProject do
   def project do
     [
       app: :server,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.11.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
